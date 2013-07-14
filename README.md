ca83bf6a4f8db1dba08b9d8510de3d3e972c1637
Vesso <v.vitanov@gmail.com>
2013-07-14 17:03:31 +0200

    Readme init + Basic Recursive

--- README.md
+++ README.md
@@ -2,3 +2,13 @@ DataStructuresAndAlgorithms
 ===========================
 
 Programming with Structure: Data Structures and Algorithms
+
+
+A set of exercises on implementaiton of common methods for data structure and algorithms.
+
+===Table of content 
+
+//Todo
+
+
+===
---
+++ Week 1/pyramid-of-aesterixs.cpp
@@ -0,0 +1,46 @@
+//non recursive implementation of the asterix pyramid
+
+#include <iostream>
+
+using namespace std;
+int main(int argc, char *argv[]) {
+	int k = 11;
+
+	    for (int i = 0; i < 10; i++)
+	    {
+//	        for(int x = 0; x <= i; x++)
+//	        {
+//	            cout << "*";
+//	        }
+//	        for(int x = 0; x < k; x++)
+//	        {
+//	            cout <<" ";
+//	        }
+//	        for(int x = 0; x < k; x++)
+//	        {
+//	            cout <<"*";
+//	        }
+//	        for(int x = 0; x <= i; x++)
+//	        {
+//	            cout << " ";
+//	        }
+	        for(int x = 0; x <= i; x++)
+	        {
+	            cout << " ";
+	        }
+	        for(int x = 0; x < k; x++)
+	        {
+	            cout <<"*";
+	        }
+	        for(int x = 0; x < k; x++)
+	        {
+	            cout <<" ";
+	        }
+	        for(int x = 0; x <= i; x++)
+	        {
+	            cout << "*";
+	        }
+	        k--;
+	        cout << endl;
+	    }
+}
